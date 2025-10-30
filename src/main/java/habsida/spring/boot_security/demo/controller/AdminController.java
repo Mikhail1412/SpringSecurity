5a9dea364652198f5d6429d9f9168f2eed50d08c
misa <mischa.zeu1998@gmail.com>
2025-10-30 20:26:47 +0900

    unable to create users

--- src/main/java/habsida/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/habsida/spring/boot_security/demo/controller/AdminController.java
@@ -8,61 +8,67 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-    @Controller
-    @RequestMapping("/admin/users")
-    public class AdminController {
-        private final UserService userService;
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final UserService userService;
 
-        public AdminController(UserService userService) {
-            this.userService = userService;
-        }
-
-        // List
-        @GetMapping
-        public String list(Model model) {
-            model.addAttribute("users", userService.findAll());
-            return "users";
-        }
-
-        // Create
-        @PostMapping
-        public String create(@Valid @ModelAttribute("user") User user,
-                             BindingResult binding) {
-            if (binding.hasErrors()) {
-                return "user-form";
-            }
-            userService.save(user);
-            return "redirect:/users";
-        }
-
-        // Edit form
-        @GetMapping("/{id}/edit")
-        public String editForm(@PathVariable Long id, Model model) {
-            User user = userService.findById(id);
-            if (user == null) {
-                return "redirect:/users";
-            }
-            model.addAttribute("user", user);
-            return "user-form";
-        }
-
-        // Update
-        @PostMapping("/{id}")
-        public String update(@PathVariable Long id,
-                             @Valid @ModelAttribute("user") User user,
-                             BindingResult binding) {
-            if (binding.hasErrors()) {
-                return "user-form";
-            }
-            user.setId(id);
-            userService.save(user);
-            return "redirect:/users";
-        }
-
-        // Delete
-        @PostMapping("/{id}/delete")
-        public String delete(@PathVariable Long id) {
-            userService.deleteById(id);
-            return "redirect:/users";
-        }
+    public AdminController(UserService userService) {
+        this.userService = userService;
     }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        model.addAttribute("users", userService.findAll());
+        return "admin/list";
+    }
+
+    @GetMapping("/users/new")
+    public String addForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("postAction", "/admin/users");
+        return "admin/add";
+    }
+
+    @PostMapping("/users")
+    public String create(@Valid @ModelAttribute("user") User user,
+                         BindingResult binding,
+                         Model model) {
+        if (binding.hasErrors()) {
+            model.addAttribute("postAction", "/admin/users");
+            return "admin/add";
+        }
+        userService.save(user);
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("/users/{id}/edit")
+    public String editForm(@PathVariable Long id, Model model) {
+        User u = userService.findById(id);
+        if (u == null) {
+            return "redirect:/admin/users";
+        }
+        model.addAttribute("user", u);
+        return "admin/edit";
+    }
+
+    @PostMapping("/users/{id}")
+    public String update(@PathVariable Long id,
+                         @Valid @ModelAttribute("user") User user,
+                         BindingResult binding,
+                         Model model) {
+        if (binding.hasErrors()) {
+            model.addAttribute("user", user);
+            return "admin/edit";
+        }
+        user.setId(id);
+        userService.save(user);
+        return "redirect:/admin/users";
+    }
+
+    @PostMapping("/users/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        userService.deleteById(id);
+        return "redirect:/admin/users";
+    }
+}
--- src/main/java/habsida/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/habsida/spring/boot_security/demo/controller/UserController.java
@@ -1,17 +1,38 @@
 package habsida.spring.boot_security.demo.controller;
 
 import habsida.spring.boot_security.demo.model.User;
+import habsida.spring.boot_security.demo.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-// Create form
 @Controller
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/register")
+    public String registerForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("postAction", "/users");
+        return "admin/add";
+    }
+
     @PostMapping
-    public String create(@Valid @ModelAttribute("user") User user, BindingResult binding) {
-    return "redirect:/admin/users";
+    public String create(@Valid @ModelAttribute("user") User user,
+                         BindingResult binding,
+                         Model model) {
+        if (binding.hasErrors()) {
+            model.addAttribute("postAction", "/users");
+            return "admin/user-add";
+        }
+        userService.save(user);
+        return "redirect:/login";
     }
 }
